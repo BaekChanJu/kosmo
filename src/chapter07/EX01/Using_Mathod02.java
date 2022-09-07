cf0f5cc53668c2f3cd3fd0125a92979bf5cf93dc
Baek Chan Ju <bcj0825@naver.com>
2022-09-07 18:24:30 +0900

    9.7 학원에서 문제내준 문제 답안

--- src/chapter07/EX01/Using_Mathod02.java
+++ src/chapter07/EX01/Using_Mathod02.java
@@ -1,5 +1,23 @@
 package chapter07.EX01;
 
+
+class Circle {
+	int r; //반지름
+	String name;
+	
+	Circle(int r,String name){//생성자함수
+		this.r = r;
+		this.name = name;
+	}
+	
+	void call() { //메소드
+		double result = 3.14 * r * r;
+		System.out.println(name + "의 넓이는 " + result + "입니다");
+	}
+	
+	
+}
+
 public class Using_Mathod02 {
 
 	public static void main(String[] args) {
@@ -7,6 +25,12 @@ public class Using_Mathod02 {
 		//	피자 : 반지름10  전체넓이를 구하셈 (r x r x 원주율)
 		// 도넛 : 반지름5
 		//피자의 넓이는 ? 입니다 , 도넛의 넓이는 ? 입니다 출력하세요
+		
+		Circle pizza = new Circle(10, "피자");
+		pizza.call();
+		
+		Circle donut = new Circle(5, "피자");
+		donut.call();
 
 	}
 
